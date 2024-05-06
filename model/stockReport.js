0891f9334e7e9a9ef0ff9c5cdb347481fb4ae895
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-06 20:08:53 +0530

    aded the date

--- model/stockReport.js
+++ model/stockReport.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const stockItemSchema = new Schema({
   date: {
     type: Date,
+    default: new Date().toISOString().split("T")[0],
   },
   comment: {
     type: String,

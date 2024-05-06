f51a3933ad4b0a4063b8edba5e59294b17475978
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-06 18:49:57 +0530

    added the salesReport

--- model/salesReport.js
+++ model/salesReport.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const salesReportSchema = new Schema({
   location: { type: String, required: true },
-  date: { type: Date, required: true, unique: true },
+  date: { type: Date, required: true },
   amount: { type: Number, required: true },
   sales: [
     {

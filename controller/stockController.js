bb647460982016ce4b0fd8345aeee995e68f22f8
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-06 19:05:11 +0530

    addded the stockController

--- controller/stockController.js
+++ controller/stockController.js
@@ -4,61 +4,48 @@ import authMiddleware from "./authMiddleware.js"; // Import the authMiddleware
 const stockController = {
   verifyToken: authMiddleware, // Middleware to verify JWT token
 
-  addSalesReport: async (req, res) => {
+  addStock: async (req, res) => {
     try {
-      const {
-        date,
-        tyreSize,
-        comment,
-        quantity,
-        amount,
-        SSP,
-        vehicle,
-        location,
-      } = req.body;
-      if (
-        !date ||
-        !tyreSize ||
-        !comment ||
-        !quantity ||
-        !amount ||
-        !SSP ||
-        !vehicle ||
-        !location
-      ) {
-        return res.status(400).json({ message: "Required fields are missing" });
-      }
+      stockController.verifyToken(req, res, async () => {
+        const { date, tyreSize, quantity, comment, SSP, location, amount } =
+          req.body;
 
-      // Get the user ID from the JWT token
-      const userId = req.userId;
-
-      // Update the existing stock report with sales information
-      let stockReport = await StockReport.findOneAndUpdate(
-        { date, status: "existing-stock" },
-        {
-          $inc: {
-            "sales.$[elem].quantity": quantity,
-            "sales.$[elem].amount": amount,
-          },
-        },
-        {
-          arrayFilters: [{ "elem.date": date }],
-          new: true,
-          upsert: true,
-        },
-      );
-
-      // If stockReport is null, it means there was no existing stock report for the date
-      if (!stockReport) {
-        return res.status(404).json({
-          message: "Stock report not found for the given date",
+        let stockReport = await StockReport.findOne({
+          date,
+          status: "existing-stock",
         });
-      }
 
-      res.status(200).json({ message: "Sales report added successfully" });
+        if (!stockReport) {
+          stockReport = new StockReport({ date, status: "existing-stock" });
+        }
+
+        const existingItemIndex = stockReport.existingStock.findIndex(
+          (item) => item.tyreSize === tyreSize,
+        );
+
+        if (existingItemIndex !== -1) {
+          // If item with the same tyreSize exists, update the quantity and amount
+          stockReport.existingStock[existingItemIndex].quantity += quantity;
+          stockReport.existingStock[existingItemIndex].amount += amount;
+        } else {
+          // If item with the same tyreSize doesn't exist, add a new entry
+          stockReport.existingStock.push({
+            tyreSize,
+            quantity,
+            comment,
+            SSP,
+            location,
+            amount,
+          });
+        }
+
+        await stockReport.save();
+
+        res.status(200).json({ message: "Stock updated successfully" });
+      });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Failed to add sales report" });
+      res.status(500).json({ error: "Failed to update stock" });
     }
   },
 

4c5c016f32357c7fb68808d2ee0941ce91820059
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-08 17:37:51 +0530

    made changes on stock-controller

--- models/sales-model.js
+++ models/sales-model.js
@@ -4,8 +4,8 @@ const salesSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   quantity: { type: Number, default: 0 },
   pricePerUnit: { type: Number, default: 0 },
+  tyreSize: String,
   customerName: String,
-  phoneNumber: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: "Worker" },
   comments: String,
 });
